6eb7f3f3b20f3b7da276a2e4a59e14cdbd844a15
Michael Chladek <mchladek@gmail.com>
2017-05-16 20:27:40 -0500

    Use environment variables for local settings

--- cjp/cjp/settings/local.py
+++ cjp/cjp/settings/local.py
@@ -1,7 +1,7 @@
 from .base import *
 
 print("---Running Local Config---")
-CJP_ADMIN_USER = "cjpadmin"
+CJP_ADMIN_USER = get_env_variable('CJP_ADMIN_USER')
 
 DEBUG = True
 
@@ -20,9 +20,9 @@ DATABASES = {
     'default': {
         #'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': 'cjpwebdb',                      # Or path to database file if using sqlite3.
-        'USER': 'cjpuser',                      # Not used with sqlite3.
-        'PASSWORD': 'cjppasswd',                  # Not used with sqlite3.
+        'NAME': get_env_variable('DATABASE_NAME'),                      # Or path to database file if using sqlite3.
+        'USER': get_env_variable('DATABASE_USER'),                      # Not used with sqlite3.
+        'PASSWORD': get_env_variable('DATABASE_PASSWORD'),                  # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
