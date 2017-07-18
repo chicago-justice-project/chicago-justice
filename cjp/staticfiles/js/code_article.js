d2af80d2095e10bac0997ec3b8f136e80548f43f
Matt Meshulam <matt@meshul.am>
2017-07-17 21:28:06 -0500

    Update location selection UI

--- cjp/staticfiles/js/code_article.js
+++ cjp/staticfiles/js/code_article.js
@@ -1,38 +1,74 @@
 var Locs = []; //array for location dictionaries
-
 var locationsSelector = "#locsHiddenInput";
 var articleBodySelector = ".articleBody";
-
 function updateForm() {
-    locsJSON = JSON.stringify(Locs);
+    var locsJSON = JSON.stringify(Locs);
     $(locationsSelector).attr("value", locsJSON);
 }
+// Adapted from: https://stackoverflow.com/questions/4811822/get-a-ranges-start-and-end-offsets-relative-to-its-parent-container
+function getSelectionWithin(element) {
+  var start = 0; // Start point of selection with relation to `element`
+  var end = 0; // End point of selection with relation to `element`
+  var collapsed = true; // Whether or not selection contains text
+  var overlap = false; // Whether the selection overlaps any current marks
+  var text = ''; // Store selected text
+  var markNodes = document.getElementsByTagName("mark");
+  var doc = element.ownerDocument || element.document;
+  var win = doc.defaultView || doc.parentWindow;
+  var sel; // To store selection
 
-function getSelectionCharOffsets() {
-  // Get and return the start and end points of the selected text, using the
-  // length of the selection to get the end rather than the range property
-  // `endOffset`
-  if (typeof window.getSelection != "undefined") {
-    selection = window.getSelection();
-    text = selection.toString();
-    range = selection.getRangeAt(0);
-    start = range.startOffset;
-    end = start + text.length;
+  if (typeof win.getSelection != "undefined") {
+    sel = win.getSelection();
+    var range = sel.getRangeAt(0);
+    var preCaretRange = range.cloneRange();
+    try {
+      text = sel.toString().replace(/[\r\n]+/g, " "); // Replace any newlines with a space
+    } catch (e) {
+      alert("Error!\n\nUnable to get selected text. See console error log for details.");
+      console.error(e);
+    }
+    if (sel.rangeCount > 0){
+      // Check for any overlaps if there are marks
+      if (markNodes.length > 0){
+        try {
+          for (var i = 0; i < markNodes.length; i++) {
+            if (sel.containsNode(markNodes[i], true)){
+              overlap = true;
+            }
+          }
+        } catch (error) { // MSIE--Not as robust as `containsNode` method but should work
+          console.warn(error);
+          try {
+            var rangeContentsFragment = range.cloneContents();
+            if (rangeContentsFragment.querySelector("mark")) {
+              overlap = true;
+            }
+          } catch (e) {
+            console.error(e);
+          }
+        }
+      }
+      preCaretRange.selectNodeContents(element);
+      var elementContents = preCaretRange.cloneContents();
+      preCaretRange.setEnd(range.startContainer, range.startOffset);
+      start = preCaretRange.toString().length;
+      preCaretRange.setEnd(range.endContainer, range.endOffset);
+      end = preCaretRange.toString().length;
+      if (end - start !== 0) {
+        collapsed = false;
+      }
+    }
   }
-  return {
-    start: start,
-    end: end,
-  };
+  return {start: start, end: end, collapsed: collapsed, overlap: overlap, text: text};
 }
-
 function updateHighlights(locsDict, element) {
   // First we need to consruct an array of ranges from Loc dictionaries as
   // markRanges() requires an array of starts and lengths
-  locRanges = [];
+  var locRanges = [];
   for (var arr in locsDict){
-    locStart = locsDict[arr]["start"];
-    locEnd = locsDict[arr]["end"];
-    locLength = locEnd - locStart;
+    var locStart = locsDict[arr]["start"];
+    var locEnd = locsDict[arr]["end"];
+    var locLength = locEnd - locStart;
     locRanges.push({'start': locStart, 'length': locLength})
   }
   // Make sure we start from the last highlight by position to the first
@@ -44,9 +80,8 @@ function updateHighlights(locsDict, element) {
     $(node).attr('id', range['start']);
   }});
 }
-
 function removeLoc(start){
-  targetObj = -1; // Number to store location of target in Locs array
+  var targetObj = -1; // Number to store location of target in Locs array
 
   // Traverse Locs array until the dictionary with the target start is found
   for (var key in Locs){
@@ -65,28 +100,22 @@ function removeLoc(start){
     $(articleBodySelector).unmark();
     updateHighlights(Locs, $(articleBodySelector));
   } else { // No target object found
-    alert("Location could not be found in Locs");
+    alert("Error!\n\nLocation could not be found in array of locations.");
   }
 }
-
-// Load and display any location data sent from server
 $(document).ready(function(){
+  // Load and display any location data sent from server
   Locs = JSON.parse($(locationsSelector).val());
   $(articleBodySelector).unmark(); // Make sure there are no marks
   updateHighlights(Locs, $(articleBodySelector));
-  setupListeners();
-});
-
-function setupListeners() {
-
   $(document).on("click", "#btnRemove", function(){
     // Get ID of <div> element that is the parent of the remove button
     // which is also the data-target of the corresponding <mark> element
-    btnId = $(this).closest('.webui-popover').attr('id');
+    var btnId = $(this).closest('.webui-popover').attr('id');
 
     // Get ID of corresponding <mark> element. This ID is the start integer
     // of the location string that has been marked
-    markId = $('mark[data-target=' + btnId + ']').attr('id');
+    var markId = $('mark[data-target=' + btnId + ']').attr('id');
     // Close popover
     $('#' + markId).webuiPopover('hide');
     // Remove that mark
@@ -100,43 +129,41 @@ function setupListeners() {
     $(this).webuiPopover('show');
   });
 
-  $(articleBodySelector).mouseup(function(){
-    // Get selected text
-    selection = window.getSelection();
-    locText = selection.toString();
+  // Change cursor when beginning to select text in article body
+  $(articleBodySelector).mousedown(function(){
+    $(this).css('cursor', 'crosshair');
+  });
 
-    // Check if selection overlaps something already marked
-    overlap = false;
-    // Get all the marks in the document
-    marks = document.getElementsByTagName("mark");
-    // If something is actually selected and there are marks, loop through them to see if the selection contains any of them
-    if (locText && marks){
-      for (var i = 0; i < marks.length; i++){
-        if (selection.containsNode(marks[i], true)){
-          overlap = true;
-          break; // An overlap has been found, so we can exit loop
-        } else {
-          continue;
-        }
-      }
+  $(articleBodySelector).mouseup(function(){
+    // Get selection
+    var locSelection = getSelectionWithin($(this).get(0));
+
+    // If there is no selected text, reset cursor and stop
+    if (locSelection.collapsed){
+      $(this).css('cursor', '');
+      return;
     }
 
     // If there is selected text and it doesn't contain a mark node, let's roll
-    if (locText && !overlap) {
-      // Confirm that the user actually wants to add the location
-      if (confirm("Add the following location?\n\n" + locText) == true){
-        $(this).unmark(); // Make sure any previous marks do not interfere
-        locStart = getSelectionCharOffsets().start;
-        locEnd = getSelectionCharOffsets().end;
-        Locs.push({'start': locStart, 'end': locEnd, 'text': locText});
+    if (!locSelection.collapsed && !locSelection.overlap) {
+      if (locSelection.text !== ''){ // Ensure text has been selected
+        Locs.push({'start': locSelection.start, 'end': locSelection.end, 'text': locSelection.text});
         updateForm();
         updateHighlights(Locs, $(this));
-      } else{ // "Cancel" is selected in confirmation popup
-        // Don't do anything
+        // Indicate newly created mark
+        $('mark#' + locSelection.start).animate({backgroundColor: '#FFF'}, 500);
+        $('mark#' + locSelection.start).animate({backgroundColor: '#FC0'}, 500);
+      } else {
+      alert("Error!\n\nThere was a problem getting the selected text.")
       }
-    } else if (overlap) { // If there is an overlap, throw an error message
+    } else if (locSelection.overlap) { // If there is an overlap, throw an error message
       alert("Error!\n\nPlease make sure your selection does not overlap any part of an already marked location.");
+    } else { // Something went wrong, likely using an unsupported browser
+      alert("Error!\n\nThe selection could not be correctly identified.");
+      updateHighlights(Locs, $(this));
     }
-  });
 
-}
+    // Reset cursor
+    $(this).css('cursor', '');
+  });
+});
