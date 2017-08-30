39b7eee31636ad52c65988391870e0ce79480f7b
Matt Meshulam <matt@meshul.am>
2017-08-29 20:41:23 -0500

    Update dump script to include column names in a file

--- scripts/dumpArticleTables.sh
+++ scripts/dumpArticleTables.sh
@@ -10,13 +10,19 @@
 DATABASE_URL=chicagojustice.cbeugrz1koxf.us-east-1.rds.amazonaws.com
 OUTPUT_DIRECTORY=/home/sftp_users/files
 
+PG_TABLES=( "newsarticles_article" "newsarticles_usercoding" "newsarticles_category" "newsarticles_usercoding_categories" )
+
 cd $OUTPUT_DIRECTORY
 if [[ $? -ne 0 ]]; then
     exit 1
 fi
 mkdir cjp_tables
-psql cjpweb_prd -h $DATABASE_URL -U cjpuser -c "\\copy newsarticles_article to 'cjp_tables/newsarticles_article.csv' with csv"
-psql cjpweb_prd -h $DATABASE_URL -U cjpuser -c "\\copy newsarticles_category to 'cjp_tables/newsarticles_category.csv' with csv"
-psql cjpweb_prd -h $DATABASE_URL -U cjpuser -c "\\copy newsarticles_article_categories to 'cjp_tables/newsarticles_article_categories.csv' with csv"
+
+for table in "${PG_TABLES[@]}"
+do
+    psql cjpweb_prd -h $DATABASE_URL -U cjpuser -c "\\copy $table to 'cjp_tables/$table.csv' with csv"
+    psql cjpweb_prd -h $DATABASE_URL -U cjpuser -c "\\d $table" >> column_names.txt
+done
+
 tar -czf cjp_tables.tgz cjp_tables/
 rm -r cjp_tables
