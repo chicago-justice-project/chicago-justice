9f65cd1e04d2ff86c2e11e3d8b58483f7d716495
Matt Meshulam <matt@meshul.am>
2017-08-30 08:11:37 -0500

    Mostly-working upgrade to Django 1.11

--- cjp/cjp/urls.py
+++ cjp/cjp/urls.py
@@ -3,6 +3,12 @@ from django.conf.urls import include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
 
+import django.contrib.auth.views
+import cjpusers.views
+import stats.views
+import crimedata.views
+import newsarticles.views
+
 urlpatterns = [
     # Examples:
     # url(r'^$', 'cjp.views.home', name='home'),
@@ -14,30 +20,30 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
 
     # user login and admin
-    url(r'^user/login/$', 'django.contrib.auth.views.login', name='loginView'),
-    url(r'^user/logout/$', 'django.contrib.auth.views.logout', kwargs={'next_page': 'mainArticleView'}, name='logoutView'),
-    url(r'^user/manage/$', 'cjpusers.views.manage', name='userManageView'),
-    url(r'^user/manage/adduser/$', 'cjpusers.views.manage', kwargs={'action': 'addUser'}, name='userManageAddUserView'),
-    url(r'^user/manage/update/(\d+)/$', 'cjpusers.views.userUpdate', name='userUpdate'),
+    url(r'^user/login/$', django.contrib.auth.views.login, name='loginView'),
+    url(r'^user/logout/$', django.contrib.auth.views.logout, kwargs={'next_page': 'mainArticleView'}, name='logoutView'),
+    url(r'^user/manage/$', cjpusers.views.manage, name='userManageView'),
+    url(r'^user/manage/adduser/$', cjpusers.views.manage, kwargs={'action': 'addUser'}, name='userManageAddUserView'),
+    url(r'^user/manage/update/(\d+)/$', cjpusers.views.userUpdate, name='userUpdate'),
 
     # toplevel article
-    url(r'^$', 'newsarticles.views.articleList'),
-    url(r'^articles/$', 'newsarticles.views.articleList', name='mainArticleView'),
+    url(r'^$', newsarticles.views.articleList),
+    url(r'^articles/$', newsarticles.views.articleList, name='mainArticleView'),
 
     #individual article
-    url(r'^articles/(\d+)/$', 'newsarticles.views.view_article', name='view-article'),
-    url(r'^articles/(\d+)/updatecoding/$', 'newsarticles.views.code_article', name='code-article'),
-    url(r'^articles/coderandom/$', 'newsarticles.views.random_article', name='random-article'),
+    url(r'^articles/(\d+)/$', newsarticles.views.view_article, name='view-article'),
+    url(r'^articles/(\d+)/updatecoding/$', newsarticles.views.code_article, name='code-article'),
+    url(r'^articles/coderandom/$', newsarticles.views.random_article, name='random-article'),
 
     # crime reports
-    url(r'^crimereports/$', 'crimedata.views.crimeReportList', name='mainCrimeReportView'),
-    url(r'^crimereports/export/$', 'crimedata.views.crimeReportExport', name='exportCrimeReport'),
+    url(r'^crimereports/$', crimedata.views.crimeReportList, name='mainCrimeReportView'),
+    url(r'^crimereports/export/$', crimedata.views.crimeReportExport, name='exportCrimeReport'),
 
     # individual crime reports
-    url(r'^crimereports/(\d+)/$', 'crimedata.views.crimeReportView', name='viewSingleCrimeReport'),
+    url(r'^crimereports/(\d+)/$', crimedata.views.crimeReportView, name='viewSingleCrimeReport'),
 
     # stats
-    url(r'^stats/totalCounts$', 'stats.views.totalCounts', name='statsTotalCounts'),
+    url(r'^stats/totalCounts$', stats.views.totalCounts, name='statsTotalCounts'),
 
 ]
 
@@ -48,4 +54,3 @@ if settings.DEBUG:
     ]
 
 urlpatterns += staticfiles_urlpatterns()
-
