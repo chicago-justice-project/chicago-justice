09f0a419de16f0ea11449559232720c07aa9e96d
Michael Chladek <mchladek@gmail.com>
2019-09-10 19:56:07 -0500

    Setup email for production

--- cjp/settings/production.py
+++ cjp/settings/production.py
@@ -39,3 +39,9 @@ DATABASES = {
         'PORT': '',
     }
 }
+
+EMAIL_HOST = get_env_variable('EMAIL_HOST')
+EMAIL_HOST_USER = get_env_variable('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = get_env_variable('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
