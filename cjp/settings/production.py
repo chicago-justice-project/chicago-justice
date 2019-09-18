3d5a3cbe4a72d3c770e9bf5614f032c8a85fb4ef
Michael Chladek <mchladek@gmail.com>
2019-09-17 19:17:59 -0500

    Set default email

--- cjp/settings/production.py
+++ cjp/settings/production.py
@@ -45,3 +45,4 @@ EMAIL_HOST_USER = get_env_variable('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = get_env_variable('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'cjpdatasite@gmail.com'
