de26691a5df5ce1e3dd32a5bed813507320e2ac6
Michael Chladek <mchladek@gmail.com>
2019-09-28 21:26:05 -0500

    Correct health check arguments

--- newsarticles/management/commands/runhealthcheck.py
+++ newsarticles/management/commands/runhealthcheck.py
@@ -11,10 +11,11 @@ class Command(BaseCommand):
     help = "Run health check of scrapers"
 
     def add_arguments(self, parser):
-        parser.add_argument('min_fails', nargs='?', type=int, default=DEFAULT_MIN_FAILS, help='Number of times scraper can fail in 24-hours before being marked as a failed scraper')
         parser.add_argument('from_email', type=str, help='Email address from which the health check report should be sent')
         parser.add_argument('to_email', nargs='+', type=str, help='Email address(es) to which the healtch check report should sent (can be more than one)')
 
+        parser.add_argument('--minfails', '-m', nargs='?', type=int, default=DEFAULT_MIN_FAILS, help='Number of times scraper can fail in 24-hours before being marked as a failed scraper')
+
     def handle(self, *args, **options):
         LOG.info('Running health check of scrapers')
-        healthcheck.run(options['min_fails'], options['from_email'], options['to_email'])
+        healthcheck.run(options['minfails'], options['from_email'], options['to_email'])
