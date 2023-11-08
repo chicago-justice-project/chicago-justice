279992c69a39c301a36e17a25180688df469346b
Michael Chladek <mchladek@gmail.com>
2023-11-07 20:14:48 -0600

    Limit random articles to first 6 months of 2023 and four categories

--- newsarticles/views.py
+++ newsarticles/views.py
@@ -388,7 +388,7 @@ def code_article(request, pk):
 
 @login_required
 def random_article(request):
-    uncoded_relevant_article_pks = Article.objects.uncoded().filter(created__gt=date(2020, 1, 1)).filter_trained_categories([13,26]).values_list('pk', flat=True)
+    uncoded_relevant_article_pks = Article.objects.uncoded().filter(created__gt=date(2023, 1, 1)).filter(created__lt=date(2023, 6, 30)).filter_trained_categories([9,10,13,26]).values_list('pk', flat=True)
     selected_pk = random.choice(uncoded_relevant_article_pks)
 
     return HttpResponseRedirect(reverse('code-article', args=(selected_pk,)))
